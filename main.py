e64601c3a8b4ce2bc6972b5b0432a89cee5a410a
Anunay Chhapre <anunaygurjar123@gmail.com>
2026-02-23 23:31:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ import geocoder
 from openai import OpenAI
 
 # API KEYS
-newsApi = "YOUR_NEWS_API_KEY"
-weather_api = "YOUR_WEATHER_API_KEY"
+newsApi = "953bcd75bb5e4225b4b676d376f0dda8"
+weather_api = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+
 
 
 #Speech + Audio setup
